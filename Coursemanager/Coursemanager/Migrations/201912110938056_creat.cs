5769752201e2ca63d8db4d06ccfa772d2d383285
zjf26 <2031285336@qq.com>
2019-12-11 17:49:30 +0800

    1

---
+++ Coursemanager/Coursemanager/Migrations/201912110938056_creat.cs
@@ -0,0 +1,94 @@
+namespace Coursemanager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class creat : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Classses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 20),
+                        TeacherId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Teachers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 20),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Stutents",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 20),
+                        ClassId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 20),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.CourseManagements",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ClassId = c.Int(nullable: false),
+                        CourseId = c.Int(nullable: false),
+                        TeacherId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ActionLinks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Controller = c.String(),
+                        Action = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Sidebars",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Controller = c.String(),
+                        Action = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Sidebars");
+            DropTable("dbo.ActionLinks");
+            DropTable("dbo.CourseManagements");
+            DropTable("dbo.Courses");
+            DropTable("dbo.Stutents");
+            DropTable("dbo.Teachers");
+            DropTable("dbo.Classses");
+        }
+    }
+}
---
+++ Coursemanager/Coursemanager/Migrations/Seeds/ActionLinkCreator.cs
@@ -0,0 +1,39 @@
+﻿using Coursemanager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coursemanager.Migrations.Seeds
+{
+    public class ActionLinkCreator
+    {
+        private readonly Coursemanager.Models.CoursemanagerEntities _context;
+        public ActionLinkCreator(Coursemanager.Models.CoursemanagerEntities context)
+        {
+            _context = context;
+        }
+        public void seed()
+        {
+            var intialActionLinks = new List<ActionLink>
+            {
+              new ActionLink
+            {
+           Name = "主页",
+            Controller="Home",
+            Action="Index"
+         
+            }
+            };
+            foreach (var action in intialActionLinks)
+            {
+                if (_context.ActionLink.Any(r => r.Name == action.Name))
+                {
+                    continue;
+                }
+                _context.ActionLink.Add(action);
+            }
+            _context.SaveChanges();
+        }
+    }
+}
