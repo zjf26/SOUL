8b6489c01f622b80b25e25102658946aeadcfa5d
zhangjianfei <2031285336@qq.com>
2019-12-13 15:42:11 +0800

    update

---
+++ Coursemanager/Coursemanager/BLLS/CourseDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coursemanager.BLLS
+{
+    public class CourseDetail
+    {
+        public string ClassName { get; set; }
+        public string CourseName { get; set; }
+        public string TeacherName { get; set; }
+
+        public TValue Name { get; set; }
+    }
+}
